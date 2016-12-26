1c7ece271ebec72a1682bf76206bae26cd81d39f
huangjingcheng <Bravowhale@Bravowhale-PC>
2016-12-26 16:45:41 +0800

    Initial commit

---
+++ src/main/java/com/hjc/shiro/Test.java
@@ -0,0 +1,10 @@
+package com.hjc.shiro;
+
+/**
+ * Created by Bravowhale on 2016/12/26.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("test success");
+    }
+}
