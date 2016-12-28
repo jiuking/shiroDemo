10b10c29573bea2e231b883a982e6909f543d969
huangjingcheng <Bravowhale@Bravowhale-PC>
2016-12-28 11:49:26 +0800

    添加相关类

--- src/main/java/com/hjc/shirodemo/shiro/SysUserFilter.java
+++ src/main/java/com/hjc/shirodemo/shiro/SysUserFilter.java
@@ -1,9 +1,13 @@
 package com.hjc.shirodemo.shiro;
 
+import com.hjc.shirodemo.common.Constants;
 import com.hjc.shirodemo.service.UserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.web.filter.PathMatchingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 
 /**
  * Created by Bravowhale on 2016/12/28.
@@ -12,8 +16,9 @@ public class SysUserFilter extends PathMatchingFilter{
     @Autowired
     private UserService userService;
 
-    @Override
-    protected boolean onPreHandler(ServletC){
-        String username = SecurityUtils.
+    protected boolean onPreHandler(ServletRequest request, ServletResponse response,Object mappedValue)throws Exception{
+        String username = (String) SecurityUtils.getSubject().getPrincipal();
+        request.setAttribute(Constants.CURRENT_USER,username);
+        return true;
     }
 }
